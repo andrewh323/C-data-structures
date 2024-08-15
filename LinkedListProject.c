196ad52c96bce60bc30a701dbab2a970ab9e552e
andrewh323 <102182355+andrewh323@users.noreply.github.com>
2024-08-15 18:14:18 -0400

    Update and rename LinkedList.c to LinkedListProject.c

--- LinkedListProject.c
+++ LinkedListProject.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct {
-    void *next;
+typedef struct Node {
+    struct Node *next;
     int data;
 } Node;
 
 Node *head = NULL;
-Node *new = NULL;
 
 void printMenu() {
     printf("You have the following options:\n");
@@ -15,36 +14,35 @@ void printMenu() {
     printf("\t2. Remove a node from the list\n");
     printf("\t3. Insert a node into the list\n");
     printf("\t4. Print the list\n");
-    printf("\t5. Quit\n");
+    printf("\t5. Reverse the list\n");
+    printf("\t6. Quit\n");
 }
 
 // add node to list
 Node *addNode(int data) {
-    if (head == NULL) {
-        new = malloc(sizeof(Node));
-        if (new == NULL) {
+    Node *newNode = malloc(sizeof(Node));
+    if (newNode == NULL) {
             return NULL;
         }
-        new->data = data;
-        head = new;
-        new->next = NULL;
+    newNode->data = data;
+    newNode->next = NULL;
+    if (head == NULL) {
+        head = newNode;
     }
     else {
-        new = malloc(sizeof(Node));
-        if (new == NULL) {
-            return NULL;
+        Node *current = head;
+        while (current->next != NULL) {
+            current = current->next;
         }
-        new->data = data;
-        new->next = head;
-        head = new;
+        current->next = newNode;
     }
-    return new;
+    return newNode;
 }
 
 // remove node from list
 int removeNode(int data) {
     Node *current = head;
-    Node *prev = head;
+    Node *prev = NULL;
     while (current != NULL) {
         if (current->data == data) {
             // if current node is the head
@@ -54,8 +52,8 @@ int removeNode(int data) {
             else {
                 prev->next = current->next;
                 free(current);
-                current = NULL;
             }
+            current = NULL;
             return 1;
         }
         prev = current;
@@ -66,43 +64,73 @@ int removeNode(int data) {
 
 // insert node into a position in list
 Node *insertNode(int data, int position) {
+    if (position == 0) {
+        Node *newNode = malloc(sizeof(Node));
+        if (newNode == NULL) {
+            return NULL;
+        }
+        newNode->data = data;
+        newNode->next = head;
+        head = newNode;
+        return newNode;
+    }
+    
     Node *current = head;
-    while (current != NULL && position != 0) {
+    while (current != NULL && position > 1) {
+        current = current->next;
         position--;
     }
-    if (position == 0) {
-        printf("Requested position too far into list\n");
+    if (current == NULL || position > 1) {
+        printf("The requested position is too far into the list\n");
         return NULL;
     }
-    Node *new = malloc(sizeof(Node));
-    if (new == NULL) {
+
+    Node *newNode = malloc(sizeof(Node));
+    if (newNode == NULL) {
         return NULL;
     }
-    new->data = data;
-    new->next = current->next;
-    current->next = new;
-    return new;
+    newNode->data = data;
+    newNode->next = current->next;
+    current->next = newNode;
+
+    return newNode;
 }
 
 // print list
 void printList() {
     Node *current = head;
     while (current != NULL) {
-        printf("%d->", current->data);
+        printf("%d", current->data);
+        if (current->next != NULL) {
+            printf("->");
+        }
         current = current->next;
     }
     printf("\n");
     return;
 }
 
+Node *reverseList(Node* head) {
+    Node *current = head;
+    Node *prev = NULL;
+    Node *next;
+    while (current != NULL) {
+        next = current->next;
+        current->next = prev;
+        prev = current;
+        current = next;
+    }
+    return prev;
+}
+
 int main(int argc, char **argv) {
     int option = -1;
     int userIn = 0;
     int userIn2 = 0;
-    while (option != 5) {
+    while (option != 6) {
         printMenu();
         int num_received = scanf("%d", &option);
-        if (num_received == 1 && option > 0 && option <= 5) {
+        if (num_received == 1 && option > 0 && option <= 6) {
             switch(option) {
                 case 1:
                     // add function
@@ -134,11 +162,15 @@ int main(int argc, char **argv) {
                     printList();
                     break;
                 case 5:
+                // reverse function
+                    head = reverseList(head);
+                    printf("The linked list has been reversed.\n");
+                    printList();
+                    break;
+                case 6:
                     break;
             }
         }
     }
-
-    printMenu();
     return 0;
 }
